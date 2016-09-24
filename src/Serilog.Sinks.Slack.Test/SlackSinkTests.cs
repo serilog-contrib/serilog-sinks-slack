bffff86cf70272cf338696249bc0ffe17479442f
mirza <mirzamerdovic@gmail.com>
2016-09-24 14:13:14 +0200

    - Serilog.Sinks.Slack.proj: Updated Serilog to latest, included needed dependency for Serilog.Sinks.PeriodicBatching and added dependency for Newtonsoft.Json.
     - SeqLoggerConfigurationExtensions: added XML comments;
     - SlackSink: Replaced RestSharp usage with HttpClient; added dictionary for colors;
    
     - Serilog.Sinks.Slack.Test: unit test project added.

---
+++ src/Serilog.Sinks.Slack.Test/SlackSinkTests.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Threading;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Serilog.Sinks.Slack.Test
+{
+    [TestClass]
+    public class SlackSinkTests
+    {
+        [TestMethod]
+        public void SendMessageTests()
+        {
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Verbose()
+                .WriteTo.Slack("xxxxxx", "#xxx", "I ghost", ":ghost:")
+                .CreateLogger();
+
+            Log.Logger.Verbose("1 Verbose");
+            Log.Logger.Debug("2 Debug");
+            Log.Logger.Error("3 Error");
+
+            try
+            {
+                throw new Exception("some logged exception!");
+            }
+            catch (Exception ex)
+            {
+                Log.Logger.Fatal(ex, "4 Fatal");
+            }
+
+            Log.Logger.Information("5 Information");
+            Log.Logger.Warning("6 Warning");
+            Log.Logger.Debug("7 Formatting {myProp}", new { myProp = "test" });
+
+            Thread.Sleep(5000);
+        }
+    }
+}
